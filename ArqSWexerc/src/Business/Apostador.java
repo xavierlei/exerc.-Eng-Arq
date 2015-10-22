039d25c1174c9b7f9664e4389be29891c2283a1e
miguelvieira123 <a62819@alunos.uminho.pt>
2015-10-22 17:50:04 +0100

    O meu primeiro commit :)

---
+++ ArqSWexerc/src/Business/Apostador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Miguel
+ */
+public class Apostador {
+    private String nome;
+    private double saldoBetCoins;
+    private ArrayList<Aposta> historicoApostas;
+    
+    
+    
+}
