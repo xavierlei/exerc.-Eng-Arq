011b43eef2294b5c96ebf0b2985264be9251e278
xavierlei <xavier.c.f@hotmail.com>
2015-10-22 15:34:23 +0100

    interface

--- ArqSWexerc/src/Presentation/BusinessPresentation.java
+++ ArqSWexerc/src/Presentation/BusinessPresentation.java
@@ -5,10 +5,24 @@
  */
 package Presentation;
 
+import Business.Aposta;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author xavier
  */
 public interface BusinessPresentation {
-    
+    public void AbrirAposta(String Eq1, String Eq2, ArrayList<Float> odd,
+            GregorianCalendar inicio, GregorianCalendar fim);
+    public void FecharAposta(String cod);
+    public void TerminarAposta(String cod, int eq1, int eq2);
+    public void AddFundo(float val);
+    public boolean RetirarFundo(float val);
+    public float ConsltarFundo();
+    public float ComprarCoins(float val);
+    public boolean CambiarCois(float val);
+    public ArrayList<Aposta> ConsultarApostas();
+    public boolean FazerApostas(String cod, String eq, float val);
 }
