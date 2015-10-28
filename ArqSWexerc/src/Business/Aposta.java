cae2b34f69382cd3b771abbe678992c8ed91a86f
miguelvieira123 <a62819@alunos.uminho.pt>
2015-10-28 23:40:49 +0000

    O meu primeiro commit :)

--- ArqSWexerc/src/Business/Aposta.java
+++ ArqSWexerc/src/Business/Aposta.java
@@ -7,6 +7,7 @@ package Business;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 /**
  *
@@ -14,9 +15,42 @@ import java.util.GregorianCalendar;
  */
 public class Aposta {
     private ArrayList<Float> odd;
-    String eq1;
-    String eq2;
-    int resultado[];
-    GregorianCalendar inicio;
-    GregorianCalendar fim;
+    private String eq1;
+    private String eq2;
+    private int resultado[];
+    private GregorianCalendar inicio;
+    private GregorianCalendar fim;
+    private HashMap<Apostador, Bid > apostadores; //key = apostador , value = Bid (que é um tuplo(valor,equipa))
+    
+    //Sets
+    public void setOdd(ArrayList<Float> odd){
+        for(Float o: odd) this.odd.add(o);
+    }
+    public void setEquipa1(String eq1) {
+        this.eq1 = eq1;
+    }
+
+    public void setEquipa2(String eq2) {
+        this.eq2 = eq2;
+    }
+
+    public void setInicio(GregorianCalendar inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setFim(GregorianCalendar fim) {
+        this.fim = fim;
+    }
+    
+    public ArrayList<Apostador> terminarAposta(int resultado[]){
+        ArrayList<Apostador> array = new ArrayList<>();
+        
+        return array;
+    }
+    
+    
+    
+    
+    
+    
 }
---
+++ ArqSWexerc/src/Business/Bid.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Bid {
+    
+    private double valorApostado;
+    private String nomeDaEquipa;
+
+    public void setValorApostado(double valorApostado) {
+        this.valorApostado = valorApostado;
+    }
+
+    public void setNomeDaEquipa(String nomeDaEquipa) {
+        this.nomeDaEquipa = nomeDaEquipa;
+    }
+
+    public double getValorApostado() {
+        return valorApostado;
+    }
+
+    public String getNomeDaEquipa() {
+        return nomeDaEquipa;
+    }
+    
+    
+    
+    
+    
+}
